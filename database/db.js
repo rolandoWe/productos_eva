a82851cd9d422c5ad2342e6c5498d38bbfc163dc
Rolando <riverarolando71@gmail.com>
2022-08-10 12:53:16 -0600

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -2,10 +2,10 @@
 const mysql= require('mysql');
 
 const conexion=mysql.createConnection({
-    host:'sql107.epizy.com',
-    user:'epiz_31522942',
-    password:'PakicSMSACr',
-    database:'epiz_31522942_productos_eva'
+    host:'',
+    user:'',
+    password:'',
+    database:''
     // host:'localhost',
     // user:'root',
     // password:'',
